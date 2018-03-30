4bdf7641900616ed7540d8cc2d5e86f3307f0e7c
andy <andy@jonezie.com>
2018-03-30 11:43:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('starting paybot')
+
+
+var fs = require("fs");
+
+var transactionManager = require('./transaction-manager');
+
+var accountConfig = require('./account.config').accounts;
+
+var owedPaymentContent = fs.readFileSync("./owed_payment.json");
+var owedPayments = JSON.parse(owedPaymentContent);
+
+async function init()
+{
+  //console.log(owedPayments)
+
+  transactionManager.init(owedPayments,accountConfig);
+
+
+
+}
+
+init();
---
+++ transaction-manager.js
@@ -0,0 +1,16 @@
+
+
+module.exports =  {
+
+  //use redis to store the past payments
+
+  async init(payments,accountConfig)
+  {
+      console.log('init transaction manager')
+
+
+
+
+  }
+
+}
